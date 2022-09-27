b480f9730b8c4cdef54b3e6327e816c421b824e7
antropushka <97852620+antropushka@users.noreply.github.com>
2022-09-27 21:42:00 +0300

    add serializable

--- src/by/epam/dragons/bean/DragonCave.java
+++ src/by/epam/dragons/bean/DragonCave.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 
 public class DragonCave implements Serializable {
 
+    private static final long serialVersionUID = 1L;
     private List<Treasure> treasures;
 
     public DragonCave() {
--- src/by/epam/dragons/bean/Treasure.java
+++ src/by/epam/dragons/bean/Treasure.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 
 public class Treasure implements Serializable {
 
+    private static final long serialVersionUID = 1L;
     private String name;
     private int price;
 
